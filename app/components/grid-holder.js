d2ffaa25c67f080cdcbfdf832ed38ed313e806c7
SilverBirchh <bradleybirch09@yahoo.co.uk>
2017-05-20 17:01:41 +0100

    Added and created grid

--- app/components/grid-holder.js
+++ app/components/grid-holder.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  xAxis: 5,
+  yAxis: 4,
+
+  init() {
+    this._super(...arguments);
+    this.get('xAxis');
+    this.get('yAxis');
+  },
+
+  xAxisArray: Ember.computed('xAxis', function() {
+    const xAxis = this.get('xAxis');
+    if (xAxis <= 0 ) {
+      this.set('xAxis', 1);
+    }
+    return Ember.A(new Array(parseInt(xAxis)).fill(null));
+  }),
+
+  yAxisArray: Ember.computed('yAxis', function() {
+    const yAxis = this.get('yAxis');
+    if (yAxis <= 0 ) {
+      this.set('yAxis', 1);
+    }
+    return Ember.A(new Array(parseInt(yAxis)).fill(null));
+  }),
+
+
 });
