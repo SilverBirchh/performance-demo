84b11130cbebc17fd29b367e77f555dc4d4c33b5
SilverBirchh <bradleybirch09@yahoo.co.uk>
2017-05-20 17:25:08 +0100

    Random colours

--- app/components/grid-square.js
+++ app/components/grid-square.js
@@ -5,6 +5,9 @@ export default Ember.Component.extend({
   classNames: ['square'],
   value: 0,
 
+  attributeBindings: ['style'],
+  style: null,
+
   didInsertElement() {
     this._super(...arguments);
     Ember.run.later(() => {
@@ -15,6 +18,9 @@ export default Ember.Component.extend({
   setRandomValue() {
     const newValue = Math.floor((Math.random() * 10) + 1);
     this.set('value', newValue);
+
+    const hexCode = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    this.set('style', Ember.String.htmlSafe(`background-color: ${hexCode}`))
     Ember.run.later(() => {
       this.setRandomValue();
     }, this.get('interval'));
